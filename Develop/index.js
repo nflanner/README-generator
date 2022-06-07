42f45f0553b037184983e113a58cafbdbb8e0c6b
Nolan Flannery <nolanflannery@gmail.com>
2022-06-06 19:05:26 -0700

    same as last commit - wrong folder previously

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,55 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+      },
+      {
+        type: 'input',
+        message: 'Please provide a description of the project:',
+        name: 'descr',
+      },
+      {
+        type: 'input',
+        message: 'Please provide installation instructions for the project:',
+        name: 'confirm',
+      },
+      {
+          type: 'input',
+          message: 'Please provide usage information for the project:',
+          name: 'confirm',
+      },
+      {
+          type: 'input',
+          message: 'Please provide contribution guidelines for the project:',
+          name: 'confirm',
+      },
+      {
+          type: 'input',
+          message: 'Please provide test instructions for the project:',
+          name: 'confirm',
+      }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    console.log('working');
+    inquirer
+        .prompt(questions)
+        .then((response) => {
+            writeToFile('./generated-README.md', response);
+            console.log('Successfully genereated README!');
+        })
+}
 
 // Function call to initialize app
 init();
